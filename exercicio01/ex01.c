86f5019e78550c820fabc0c96e8dc8c0a9bc426b
Thiago Assis <55933312+thiagoassisk8@users.noreply.github.com>
2020-10-07 17:12:20 -0300

    [add] - atualização do ex01

--- exercicio01/ex01.c
+++ exercicio01/ex01.c
@@ -1,9 +1,47 @@
-#include <stdio.h>
+    #include <pthread.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+     
+    void *PrintHello(void *threadid){//função a ser executada quando uma thread for criada
+      long id;
+      id = (long)threadid;//conversão de void pra long
+      printf("Thread %ld: Olá mundo!\n", id);
+      pthread_exit(NULL); //finalização da thread
+    }
+     
+    int threads(){
+      pthread_t t1, t2,t3; //criando t1, t2 e t3 do tipo thread
+      int create; //variável que recebe o retorno da função pthread_create()
+      long num; //identificador da thread
+      for (int i = 0; i < 1000; i++) {
+      num = 1;
+      printf("Main: criando a thread %ld\n", num);
+      create = pthread_create(&t1, NULL, PrintHello, (void *)num); //criando thread 1
+        
+      num = 2;
+      printf("Main: criando a thread %ld\n", num);
+      create = pthread_create(&t1, NULL, PrintHello, (void *)
+      num); //criando thread 2
 
-int main(){
-    for (int i = 0; i < 10; i++) {
-        printf("%d \n processo n�mero", i);
+      num = 3;
+      printf("Main: criando a thread %ld\n", num);
+      create = pthread_create(&t1, NULL, PrintHello, (void *)
+      num); //criando thread 2        
+             
+    
+  }
+           
+    }
+
+    int main(void){
+    for (int i = 0; i < 1000; i++) {
+        threads();
+
+        printf("%d \n processo número", i);
     }
 
     return 0;
 }
+
+
